237eaf3f74f6b7a656084ce7931b08df433f39f1
pozuhtuhv <pozuhtuhv@gmail.com>
2025-07-04 15:52:27 +0900

    (upload)base

---
+++ data/questions.js
@@ -0,0 +1,56 @@
+const questions = [
+  {
+    question: "1. 나는 사람들과 함께 있을 때 에너지를 얻는다.",
+    options: [
+      { text: "매우 그렇다", type: "E" },
+      { text: "약간 그렇다", type: "E" },
+      { text: "약간 아니다", type: "I" },
+      { text: "전혀 아니다", type: "I" }
+    ]
+  },
+  {
+    question: "2. 나는 현실적이고 구체적인 것을 선호한다.",
+    options: [
+      { text: "매우 그렇다", type: "S" },
+      { text: "약간 그렇다", type: "S" },
+      { text: "약간 아니다", type: "N" },
+      { text: "전혀 아니다", type: "N" }
+    ]
+  },
+    {
+        question: "3. 나는 계획을 세우고 따르는 것을 좋아한다.",
+        options: [
+        { text: "매우 그렇다", type: "J" },
+        { text: "약간 그렇다", type: "J" },
+        { text: "약간 아니다", type: "P" },
+        { text: "전혀 아니다", type: "P" }
+        ]
+    },
+    {
+        question: "4. 나는 새로운 아이디어나 가능성에 열려 있다.",
+        options: [
+        { text: "매우 그렇다", type: "N" },
+        { text: "약간 그렇다", type: "N" },
+        { text: "약간 아니다", type: "S" },
+        { text: "전혀 아니다", type: "S" }
+        ]
+    },
+    {
+        question: "3. 나는 계획을 세우고 따르는 것을 좋아한다.",
+        options: [
+        { text: "매우 그렇다", type: "J" },
+        { text: "약간 그렇다", type: "J" },
+        { text: "약간 아니다", type: "P" },
+        { text: "전혀 아니다", type: "P" }
+        ]
+    },
+    {
+        question: "4. 나는 새로운 아이디어나 가능성에 열려 있다.",
+        options: [
+        { text: "매우 그렇다", type: "N" },
+        { text: "약간 그렇다", type: "N" },
+        { text: "약간 아니다", type: "S" },
+        { text: "전혀 아니다", type: "S" }
+        ]
+    }
+];
